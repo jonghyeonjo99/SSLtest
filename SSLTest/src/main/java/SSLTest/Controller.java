85c6f19cc761460970fac5903c453f44487f8ae1
Jong hyeon Jo <97875266+jonghyeonjo99@users.noreply.github.com>
2023-11-30 14:06:20 +0900

    Add files via upload

---
+++ SSLTest/src/main/java/SSLTest/Controller.java
@@ -0,0 +1,13 @@
+package SSLTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "SSL test webpage";
+    }
+}
